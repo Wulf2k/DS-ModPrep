4a9554f22d2a776cd8c323b7b3870d981454472a
Wulf2k <wulf2k@gmail.com>
2016-08-04 04:17:32 -0500

    .

---
+++ DS-ModPrep/frmModPrep.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmModPrep
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtEXEfile = New System.Windows.Forms.TextBox()
+        Me.lblGAFile = New System.Windows.Forms.Label()
+        Me.btnModify = New System.Windows.Forms.Button()
+        Me.txtInfo = New System.Windows.Forms.TextBox()
+        Me.btnExtractBNDs = New System.Windows.Forms.Button()
+        Me.btnExtractDCX = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtEXEfile
+        '
+        Me.txtEXEfile.AllowDrop = True
+        Me.txtEXEfile.Location = New System.Drawing.Point(68, 12)
+        Me.txtEXEfile.Name = "txtEXEfile"
+        Me.txtEXEfile.ReadOnly = True
+        Me.txtEXEfile.Size = New System.Drawing.Size(671, 20)
+        Me.txtEXEfile.TabIndex = 29
+        Me.txtEXEfile.Text = "Drag 'n Drop DARKSOULS.exe here"
+        '
+        'lblGAFile
+        '
+        Me.lblGAFile.AutoSize = True
+        Me.lblGAFile.Location = New System.Drawing.Point(12, 15)
+        Me.lblGAFile.Name = "lblGAFile"
+        Me.lblGAFile.Size = New System.Drawing.Size(50, 13)
+        Me.lblGAFile.TabIndex = 30
+        Me.lblGAFile.Text = "EXE File:"
+        '
+        'btnModify
+        '
+        Me.btnModify.Enabled = False
+        Me.btnModify.Location = New System.Drawing.Point(68, 38)
+        Me.btnModify.Name = "btnModify"
+        Me.btnModify.Size = New System.Drawing.Size(80, 23)
+        Me.btnModify.TabIndex = 31
+        Me.btnModify.Text = "Modify EXE"
+        Me.btnModify.UseVisualStyleBackColor = True
+        '
+        'txtInfo
+        '
+        Me.txtInfo.Location = New System.Drawing.Point(15, 202)
+        Me.txtInfo.Multiline = True
+        Me.txtInfo.Name = "txtInfo"
+        Me.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtInfo.Size = New System.Drawing.Size(805, 142)
+        Me.txtInfo.TabIndex = 32
+        '
+        'btnExtractBNDs
+        '
+        Me.btnExtractBNDs.Enabled = False
+        Me.btnExtractBNDs.Location = New System.Drawing.Point(154, 38)
+        Me.btnExtractBNDs.Name = "btnExtractBNDs"
+        Me.btnExtractBNDs.Size = New System.Drawing.Size(80, 23)
+        Me.btnExtractBNDs.TabIndex = 33
+        Me.btnExtractBNDs.Text = "Extract BNDs"
+        Me.btnExtractBNDs.UseVisualStyleBackColor = True
+        '
+        'btnExtractDCX
+        '
+        Me.btnExtractDCX.Enabled = False
+        Me.btnExtractDCX.Location = New System.Drawing.Point(240, 38)
+        Me.btnExtractDCX.Name = "btnExtractDCX"
+        Me.btnExtractDCX.Size = New System.Drawing.Size(80, 23)
+        Me.btnExtractDCX.TabIndex = 34
+        Me.btnExtractDCX.Text = "Extract DCXs"
+        Me.btnExtractDCX.UseVisualStyleBackColor = True
+        '
+        'frmModPrep
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(832, 356)
+        Me.Controls.Add(Me.btnExtractDCX)
+        Me.Controls.Add(Me.btnExtractBNDs)
+        Me.Controls.Add(Me.txtInfo)
+        Me.Controls.Add(Me.btnModify)
+        Me.Controls.Add(Me.txtEXEfile)
+        Me.Controls.Add(Me.lblGAFile)
+        Me.Name = "frmModPrep"
+        Me.Text = "Wulf's Dark Souls Mod Prepper"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtEXEfile As TextBox
+    Friend WithEvents lblGAFile As Label
+    Friend WithEvents btnModify As Button
+    Friend WithEvents txtInfo As TextBox
+    Friend WithEvents btnExtractBNDs As Button
+    Friend WithEvents btnExtractDCX As Button
+End Class
---
+++ DS-ModPrep/frmModPrep.vb
@@ -0,0 +1,514 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+
+Public Class frmModPrep
+    Public EXEver As String = "unknown"
+    Public dataPath As String = ""
+    Public bigEndian = False
+
+    Private Sub WriteBytes(ByRef fs As FileStream, ByVal loc As Integer, ByVal byt() As Byte)
+        fs.Position = loc
+        For i = 0 To byt.Length - 1
+            fs.WriteByte(byt(i))
+        Next
+    End Sub
+
+    Private Sub CreateFolders()
+        If Not Directory.Exists("C:\FRPG") Then
+            Directory.CreateDirectory("C:\FRPG")
+            Directory.CreateDirectory("C:\FRPG\data")
+            Directory.CreateDirectory("C:\FRPG\data\CAPTURE")
+            Directory.CreateDirectory("C:\FRPG\data\CAPTURE\breakobj")
+            Directory.CreateDirectory("C:\FRPG\data\CAPTURE\dbgreport")
+            Directory.CreateDirectory("C:\FRPG\data\CAPTURE\EnvLightMap")
+            Directory.CreateDirectory("C:\FRPG\data\CAPTURE\event")
+            Directory.CreateDirectory("C:\FRPG\data\CAPTURE\mapstudio")
+            Directory.CreateDirectory("C:\FRPG\data\CAPTURE\param")
+            Directory.CreateDirectory("C:\FRPG\data\CAPTURE\Replay")
+            Directory.CreateDirectory("C:\FRPG\data\CAPTURE\screenshot")
+            Directory.CreateDirectory("C:\FRPG\data\CAPTURE\win32savedata")
+            Directory.CreateDirectory("C:\FRPG\data\INTERROOT_win32")
+            Directory.CreateDirectory("C:\FRPG\data\INTERROOT_win32\script")
+            Directory.CreateDirectory("C:\FRPG\data\INTERROOT_win32\script\ai")
+            Directory.CreateDirectory("C:\FRPG\data\INTERROOT_win32\script\ai\out")
+            Directory.CreateDirectory("C:\FRPG\data\INTERROOT_win32\script\talk")
+            Directory.CreateDirectory("C:\FRPG\data\INTERROOT_win32\sfx")
+            Directory.CreateDirectory("C:\FRPG\data\INTERROOT_win32\sfx\effects")
+            Directory.CreateDirectory("C:\FRPG\data\INTERROOT_win32\sfx\lua")
+            Directory.CreateDirectory("C:\FRPG\data\INTERROOT_win32\sfx\model")
+            Directory.CreateDirectory("C:\FRPG\data\INTERROOT_win32\sfx\tex")
+            Directory.CreateDirectory("C:\FRPG\data\INTERROOT_win32\sfx\debug")
+            Directory.CreateDirectory(dataPath & "\testdata")
+            txtInfo.Text = txtInfo.Text & "Created c:\FRPG and game:/testdata folders" & Environment.NewLine
+        End If
+    End Sub
+    Private Sub modReleaseEXE(ByRef EXEstream As FileStream)
+        Dim byt() As Byte
+
+        If Not File.Exists(txtEXEfile.Text & ".release.bak") Then
+            File.Copy(txtEXEfile.Text, txtEXEfile.Text & ".release.bak")
+            txtInfo.Text = txtInfo.Text & "Created " & txtEXEfile.Text & ".release.bak" & Environment.NewLine
+        Else
+            txtInfo.Text = txtInfo.Text & txtEXEfile.Text & ".release.bak already exists" & Environment.NewLine
+        End If
+
+
+        byt = System.Text.Encoding.Unicode.GetBytes("dvdroot:")
+
+        'DVDBND0:
+        WriteBytes(EXEstream, &HD65EA4, byt)
+        WriteBytes(EXEstream, &HD660F8, byt)
+        WriteBytes(EXEstream, &HD66180, byt)
+        WriteBytes(EXEstream, &HD6627C, byt)
+        WriteBytes(EXEstream, &HD662C8, byt)
+        WriteBytes(EXEstream, &HD66318, byt)
+        WriteBytes(EXEstream, &HD66C90, byt)
+
+        'DVDBND1:
+        WriteBytes(EXEstream, &HD57F14, byt)
+        WriteBytes(EXEstream, &HD65DAC, byt)
+        WriteBytes(EXEstream, &HD65DF0, byt)
+        WriteBytes(EXEstream, &HD65FFC, byt)
+        WriteBytes(EXEstream, &HD6613C, byt)
+        WriteBytes(EXEstream, &HD6636C, byt)
+        WriteBytes(EXEstream, &HD66484, byt)
+        WriteBytes(EXEstream, &HD665F0, byt)
+        WriteBytes(EXEstream, &HD666E4, byt)
+
+        'DVDBND2:
+        WriteBytes(EXEstream, &HD66734, byt)
+        WriteBytes(EXEstream, &HD667C4, byt)
+        WriteBytes(EXEstream, &HD66EAC, byt)
+
+        'DVDBND3:
+        WriteBytes(EXEstream, &HD65E34, byt)
+        WriteBytes(EXEstream, &HD663B0, byt)
+        WriteBytes(EXEstream, &HD66434, byt)
+
+
+        byt = System.Text.Encoding.Unicode.GetBytes("C:")
+
+        'N:
+        WriteBytes(EXEstream, &HD65BB4, byt)
+        WriteBytes(EXEstream, &HD65C3C, byt)
+        WriteBytes(EXEstream, &HD6FBA0, byt)
+    End Sub
+    Private Sub modDebugEXE(ByRef EXEstream As FileStream)
+        Dim byt() As Byte
+        If Not File.Exists(txtEXEfile.Text & ".debug.bak") Then
+            File.Copy(txtEXEfile.Text, txtEXEfile.Text & ".debug.bak")
+            txtInfo.Text = txtInfo.Text & "Created " & txtEXEfile.Text & ".debug.bak" & Environment.NewLine
+        Else
+            txtInfo.Text = txtInfo.Text & txtEXEfile.Text & ".debug.bak already exists" & Environment.NewLine
+        End If
+
+
+
+        byt = System.Text.Encoding.Unicode.GetBytes("dvdroot:")
+
+        'DVDBND0:
+        WriteBytes(EXEstream, &HD6816C, byt)
+        WriteBytes(EXEstream, &HD683C0, byt)
+        WriteBytes(EXEstream, &HD68448, byt)
+        WriteBytes(EXEstream, &HD68544, byt)
+        WriteBytes(EXEstream, &HD68590, byt)
+        WriteBytes(EXEstream, &HD685E0, byt)
+        WriteBytes(EXEstream, &HD68F58, byt)
+
+        'DVDBND1:
+        WriteBytes(EXEstream, &HD5C2D4, byt)
+        WriteBytes(EXEstream, &HD68074, byt)
+        WriteBytes(EXEstream, &HD680BC, byt)
+        WriteBytes(EXEstream, &HD682C4, byt)
+        WriteBytes(EXEstream, &HD68404, byt)
+        WriteBytes(EXEstream, &HD68634, byt)
+        WriteBytes(EXEstream, &HD6874C, byt)
+        WriteBytes(EXEstream, &HD688B8, byt)
+        WriteBytes(EXEstream, &HD689AC, byt)
+
+        'DVDBND2:
+        WriteBytes(EXEstream, &HD689FC, byt)
+        WriteBytes(EXEstream, &HD68A8C, byt)
+        WriteBytes(EXEstream, &HD69174, byt)
+
+        'DVDBND3:
+        WriteBytes(EXEstream, &HD680FC, byt)
+        WriteBytes(EXEstream, &HD68678, byt)
+        WriteBytes(EXEstream, &HD686FC, byt)
+
+
+        byt = System.Text.Encoding.Unicode.GetBytes("C:")
+
+        'N:
+        WriteBytes(EXEstream, &HD67E7C, byt)
+        WriteBytes(EXEstream, &HD67F04, byt)
+        WriteBytes(EXEstream, &HD71ED0, byt)
+
+    End Sub
+    Private Sub btnModify_Click(sender As Object, e As EventArgs) Handles btnModify.Click
+        CreateFolders()
+
+        Dim EXEstream = New IO.FileStream(txtEXEfile.Text, IO.FileMode.Open)
+        Select Case EXEver
+            Case "release"
+                modReleaseEXE(EXEstream)
+            Case "debug"
+                modDebugEXE(EXEstream)
+        End Select
+        EXEstream.Dispose()
+
+
+    End Sub
+
+
+
+
+    Private Sub txt_Drop(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles txtEXEfile.DragDrop
+        Dim file() As String = e.Data.GetData(DataFormats.FileDrop)
+        sender.Text = file(0)
+
+        Dim EXEstream = New IO.FileStream(file(0), IO.FileMode.Open)
+        EXEstream.Position = &H80
+
+        Dim verCheck As Byte
+        verCheck = EXEstream.ReadByte
+        btnModify.Enabled = True
+        btnExtractBNDs.Enabled = True
+        btnExtractDCX.Enabled = True
+
+        dataPath = Microsoft.VisualBasic.Left(txtEXEfile.Text, InStrRev(txtEXEfile.Text, "\"))
+
+        Select Case verCheck
+            Case &H54
+                EXEver = "release"
+            Case &HB4
+                EXEver = "debug"
+            Case &HE2
+                EXEver = "beta"
+                btnModify.Enabled = False
+                btnExtractBNDs.Enabled = False
+                btnExtractDCX.Enabled = False
+            Case Else
+                EXEver = "unknown"
+                btnModify.Enabled = False
+                btnExtractBNDs.Enabled = False
+                btnExtractDCX.Enabled = False
+        End Select
+
+        txtInfo.Text = txtInfo.Text & "EXE type: " & EXEver & Environment.NewLine
+        EXEstream.Dispose()
+    End Sub
+    Private Sub txt_DragEnter(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles txtEXEfile.DragEnter
+        e.Effect = DragDropEffects.Copy
+    End Sub
+
+    Private Sub ExtractBHD5(ByRef filename As String)
+        Dim BHDstream As New IO.FileStream(dataPath & filename & ".bhd5", IO.FileMode.Open)
+        Dim BDTstream As New IO.FileStream(dataPath & filename & ".bdt", IO.FileMode.Open)
+
+        Dim BHDoffset As Integer
+
+        Dim numFiles As Integer = 0
+
+        Dim currFileSize As Integer = 0
+        Dim currFileOffset As Integer = 0
+        Dim currFileID As Integer = 0
+        Dim currFileNameOffset As Integer = 0
+        Dim currFileBytes() As Byte = {}
+        Dim currFileName As String = ""
+        Dim currFilePath As String = ""
+
+        Dim count As Integer = 0
+
+        Dim idx As Integer
+        Dim fileidx() As String = My.Resources.fileidx.Replace(Chr(&HD), "").Split(Chr(&HA))
+        Dim hashidx(fileidx.Length - 1) As UInteger
+
+        For i = 0 To fileidx.Length - 1
+            hashidx(i) = HashFileName(fileidx(i))
+        Next
+
+
+        bigEndian = False
+
+        numFiles = Int32FromStream(BHDstream, &H10)
+
+        For i As Integer = 0 To numFiles - 1
+            count = Int32FromStream(BHDstream, &H18 + i * &H8)
+            BHDoffset = Int32FromStream(BHDstream, &H1C + i * 8)
+
+            For j = 0 To count - 1
+                currFileSize = Int32FromStream(BHDstream, BHDoffset + &H4)
+                currFileOffset = Int32FromStream(BHDstream, BHDoffset + &H8)
+
+                ReDim currFileBytes(currFileSize - 1)
+
+                BDTstream.Position = currFileOffset
+                For k = 0 To currFileSize - 1
+                    currFileBytes(k) = BDTstream.ReadByte
+                Next
+
+                currFileName = ""
+
+                If hashidx.Contains(UInt32FromStream(BHDstream, BHDoffset)) Then
+                    idx = Array.IndexOf(hashidx, UInt32FromStream(BHDstream, BHDoffset))
+                    currFileName = fileidx(idx)
+                    currFileName = currFileName.Replace("/", "\")
+                Else
+                    currFileName = "\NOMATCH\" & Hex(BHDoffset)
+                End If
+
+                currFileName = Microsoft.VisualBasic.Left(dataPath, dataPath.Length - 1) & currFileName
+                currFilePath = Microsoft.VisualBasic.Left(currFileName, InStrRev(currFileName, "\"))
+
+                If (Not System.IO.Directory.Exists(currFilePath)) Then
+                    System.IO.Directory.CreateDirectory(currFilePath)
+                End If
+
+                File.WriteAllBytes(currFileName, currFileBytes)
+
+                BHDoffset += &H10
+            Next
+        Next
+        BDTstream.Dispose()
+        BHDstream.Dispose()
+    End Sub
+    Private Sub ExtractDFLT(ByRef filename As String)
+        Dim DCXstream As New IO.FileStream(filename, IO.FileMode.Open)
+        Dim currFileName As String = ""
+        bigEndian = True
+
+        Dim startOffset As UInteger = Int32FromStream(DCXstream, &H14) + &H22
+
+        Dim newbytes(Int32FromStream(DCXstream, &H20) - 1) As Byte
+        Dim decbytes(Int32FromStream(DCXstream, &H1C)) As Byte
+
+        DCXstream.Position = startOffset
+        For i = 0 To newbytes.Length - 2
+            newbytes(i) = DCXstream.ReadByte
+
+        Next
+
+        decbytes = Decompress(newbytes)
+
+        currFileName = Microsoft.VisualBasic.Left(filename, filename.Length - &H4)
+
+        File.WriteAllBytes(currFileName, decbytes)
+
+        DCXstream.Dispose()
+    End Sub
+
+    Private Sub btnExtractBNDs_Click(sender As Object, e As EventArgs) Handles btnExtractBNDs.Click
+        ExtractBHD5("dvdbnd0")
+        txtInfo.Text = txtInfo.Text & "DVDBND0 extracted." & Environment.NewLine
+
+        ExtractBHD5("dvdbnd1")
+        txtInfo.Text = txtInfo.Text & "DVDBND1 extracted." & Environment.NewLine
+
+        ExtractBHD5("dvdbnd2")
+        txtInfo.Text = txtInfo.Text & "DVDBND2 extracted." & Environment.NewLine
+
+        ExtractBHD5("dvdbnd3")
+        txtInfo.Text = txtInfo.Text & "DVDBND3 extracted." & Environment.NewLine
+
+    End Sub
+    Private Function HashFileName(filename As String) As UInteger
+
+        REM This code copied from https://github.com/Burton-Radons/Alexandria
+
+        If filename Is Nothing Then
+            Return 0
+        End If
+
+        Dim hash As UInteger = 0
+
+        For Each ch As Char In filename
+            hash = hash * &H25 + Asc(Char.ToLowerInvariant(ch))
+        Next
+
+        Return hash
+    End Function
+    Private Function ASCIIStrFromStream(ByRef fs As FileStream, ByVal loc As UInteger) As String
+        Dim Str As String = ""
+        Dim cont As Boolean = True
+        Dim byt As Byte
+
+        fs.Position = loc
+
+        While cont
+            byt = fs.ReadByte
+
+            If byt > 0 Then
+                Str = Str + Convert.ToChar(byt)
+            Else
+                cont = False
+            End If
+        End While
+
+        Return Str
+    End Function
+    Private Function Int32FromStream(ByRef fs As FileStream, ByVal loc As Integer) As Integer
+        Dim tmpInt As Integer = 0
+        Dim byt As Byte
+
+        fs.Position = loc
+
+        If bigEndian Then
+            For i = 0 To 3
+                byt = fs.ReadByte
+                tmpInt += Convert.ToInt32(byt) * &H100 ^ (3 - i)
+            Next
+        Else
+            For i = 0 To 3
+                byt = fs.ReadByte
+                tmpInt += Convert.ToInt32(byt) * &H100 ^ i
+            Next
+        End If
+
+
+        Return tmpInt
+    End Function
+    Private Function UInt32FromStream(ByRef fs As FileStream, ByVal loc As Integer) As UInteger
+        Dim tmpUInt As UInteger = 0
+        Dim byt As Byte
+
+        fs.Position = loc
+        If bigEndian Then
+            For i = 0 To 3
+                byt = fs.ReadByte
+                tmpUInt += Convert.ToUInt32(byt) * &H100 ^ (3 - i)
+            Next
+        Else
+            For i = 0 To 3
+                byt = fs.ReadByte
+                tmpUInt += Convert.ToUInt32(byt) * &H100 ^ i
+            Next
+        End If
+
+
+        Return tmpUInt
+    End Function
+    Public Function Decompress(ByVal cmpBytes() As Byte) As Byte()
+        Dim sourceFile As MemoryStream = New MemoryStream(cmpBytes)
+        Dim destFile As MemoryStream = New MemoryStream()
+        Dim compStream As New DeflateStream(sourceFile, CompressionMode.Decompress)
+        Dim myByte As Integer = compStream.ReadByte()
+
+        While myByte <> -1
+            destFile.WriteByte(CType(myByte, Byte))
+            myByte = compStream.ReadByte()
+        End While
+
+        destFile.Close()
+        sourceFile.Close()
+
+        Return destFile.ToArray
+    End Function
+    Private Sub btnExtractDCX_Click(sender As Object, e As EventArgs) Handles btnExtractDCX.Click
+        Dim dcxlist() As String = Directory.GetFiles(dataPath, "*.dcx", SearchOption.AllDirectories)
+
+        For Each dcx In dcxlist
+            ExtractDFLT(dcx)
+        Next
+
+        txtInfo.Text = txtInfo.Text & "DCXs extracted" & Environment.NewLine
+    End Sub
+End Class
+
+
+
+
+'List of Folder Aliases
+'game = system:/
+'debug = game:/
+'capture = N:/FRPG/data/CAPTURE
+'dvdroot
+'interroot = N:/FRPG/data/INTERROOT_win32
+'model = dvdroot:/testdata
+'animation
+'texture
+'material
+'loadlist
+'shader = dvdbnd1:/shader
+'font = dvdbnd1:/font
+'msg = dvdbnd3:/msg
+'msgpatch
+'chr = dvdbnd0:/chr
+'chrtpf
+'chrflver
+'chranibnd
+'chrhkx
+'chresdpatch
+'obj = dvdbnd1:/obj
+'objflver
+'objtpf
+'objhkx
+'objanibnd
+'item = dvdbnd0:/item
+'parts = dvdbnd1:/parts
+'map = dvdbnd0:/map
+'mappatch
+'maptpf
+'mapflver
+'maphkx
+'remo = dvdbnd0:/remo
+'breakobj = 'dvdbnd0:/map/breakobj
+'msb = dvdbnd0:/map/mapstudio
+'mtd = dvdbnd1:/mtd
+'param = dvdbnd3:/param
+'parampatch
+'paramdef = dvdbnd3:/paramdef
+'facegen = dvdbnd1:/facegen
+'movjp = dvdroot:/movjp
+'movna = dvdroot:/movna
+'moveu = dvdroot:/moveu
+'movww = dvdroot:/movww
+'sound = dvdbnd1:/sound
+'sndmap
+'sndchr
+'sndremo
+'fmod
+'other = dvdbnd1:/other
+'luascript = dvdbnd2:/script
+'luascriptpatch
+'talkscript = dvdbnd2:/script/talk
+'talkscriptpatch
+'navimesh
+'dbgscript = interroot:/script
+'dbgai = interroot:/script/ai
+'cap_param = capture:/param
+'cap_breakobj = capture:/breakobj
+'cap_screenshot = capture:/screenshot
+'cap_dbgrep = capture:/dbgreport
+'cap_mapstudio = capture:/mapstudio
+'cap_EnvLightMap = capture:/EnvLightMap
+'replay = capture:/Replay
+'config = dvdroot:/
+'ffx = interroot:/sfx/effects
+'ffxlua = interroot:/sfx/lua
+'ffxbnd = dvdbnd0:/sfx
+'ffxbndpatch
+'ffxmodel = interroot:/sfx/model
+'ffxtex = interroot:/sfx/tex
+'ffxdebug = interroot:/sfx/debug
+'menutexture
+'menutexpatch
+'menuesd
+'event = dvdbnd2:/event
+'eventpatch
+'cap_event = capture:/event
+'fmod_dlc
+'sound_dlc
+'sndmap_dlc
+'sndchr_dlc
+'sndremo_dlc
+'maptpf_dlc
+'map_dlc
+'mtd_dlc
+'remobnd_dlc
+'menu
+'menutex_dlc
+'menuesd_dlc
+'other_dlc
+'chranibnd_dlc
+'breakobj_dlc
